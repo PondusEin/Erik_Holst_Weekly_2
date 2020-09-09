5214df5d12459a882f8e1e79a40f566db2c48b7a
PondusEin <PondusEin@users.noreply.github.com>
2020-09-09 14:15:04 +0200

    Fixed all task to match corresponding assignment. Started on void menu()

--- Erik_Holst_Weekly_2/Erik_Holst_Weekly_2.cpp
+++ Erik_Holst_Weekly_2/Erik_Holst_Weekly_2.cpp
@@ -20,6 +20,7 @@ int getintfromuser() {
 }
 
 
+
 void task1() 
 {
     //Output immediatly
@@ -61,16 +62,8 @@ void task2() {
 }
 
 void task3() {
-    std::cout << "3. Seems like there's nothing here.\n\n\n" << std::endl;
 
-    std::cout << "Press any button to go back.\n\n\n" << std::endl;
-    char done3 = _getch();
-}
-
-
-void task4() {
-
-    std::cout << "4. Make a program that writes 5 to 50 in steps of 5. Use a while loop.\n" << std::endl;
+    std::cout << "3. Make a program that writes 5 to 50 in steps of 5. Use a while loop.\n" << std::endl;
     int count5{ 5 };
     while (count5 < 51)
     {
@@ -79,98 +72,98 @@ void task4() {
         count5 += 5;
     }
     std::cout << "\nDone! This was while-loop!\n\n\n";
-    char done4 = _getch();
+    char done3 = _getch();
 }
 
-void task5() {
-    std::cout << "5. Make a program that writes 5 to 50 in steps of 5. Use a for loop.\n" << std::endl;
+
+void task4() {
+    std::cout << "4. Make a program that writes 5 to 50 in steps of 5. Use a for loop.\n" << std::endl;
     for (int count6{ 5 }; count6 <= 50; count6 += 5)
     {
         std::cout << count6 << ' ';
     }
 
     std::cout << "\nDone! This was for-loop!\n\n\n";
+    char done4 = _getch();
+}
+
+
+
+void task5() {
+    std::cout << "5. Make a program that writes 5 to 50 in steps of 5. Use..while." << '\n' << "TBA\n\n" << std::endl;
+
+    int count5{ 5 };
+    while (count5 <= 50)
+    {
+        std::cout << count5 << ' ';
+        count5 == 50;
+        count5 += 5;
+    }
+    std::cout << "\nDone! This was while-loop!\n\n\n";
     char done5 = _getch();
 }
 
 
 
 void task6() {
-    std::cout << "6. Make a program that writes 5 to 50 in steps of 5. Use..while." << '\n' << "TBA\n\n" << std::endl;
-
-
-    char done6 = _getch();
-}
-
-
-/*
-void task7() {
    
-    std::cout << "7. Make a program that ask user what grade they want in Programming 1. Use switch.\n" << std::endl;
+    std::cout << "6. Make a program that ask user what grade they want in Programming 1. Use switch.\n" << std::endl;
 
-    enum class Grade
-    {
-        A,
-        B,
-        C,
-        D,
-        E,
-        F
-    };
-    void printGrade(Grade grade);
+    char grade;
+    std::cout << "What grade do you want in Programming 1? [A-F]: ";
+    std::cin >> grade;
+
     {
 
 
         switch (grade) {
 
-        case Grade::A:
-            std::cout << "Outstanding";
+        case 'A': case 'a':
+            std::cout << "Outstanding" << std::endl;
             break;
-        case Grade::B:
-            std::cout << "Very good";
+        case 'B': case 'b':
+            std::cout << "Very good" << std::endl;
             break;
-        case Grade::C:
-            std::cout << "Good";
+        case 'C': case 'c':
+            std::cout << "Good" << std::endl;
             break;
-        case Grade::D:
-            std::cout << "Some flaws";
+        case 'D': case 'd':
+            std::cout << "Some flaws" << std::endl;
             break;
-        case Grade::E:
-            std::cout << "Not very good...";
+        case 'E': case 'e':
+            std::cout << "Not very good..." << std::endl;
             break;
-        case Grade::F:
-            std::cout << "Fail";
+        case 'F': case 'f':
+            std::cout << "Fail" << std::endl;
             break;
         default:
-            std::cout << "That is not a grade!";
+            std::cout << "That is not a grade!" << std::endl;
             break;
         }
     }
 
     std::cout << "\n\nWell done! Press any button to continue." << std::endl;
-    char done7 = _getch();
+    char done6 = _getch();
 }
-*/
 
+void task7() {
+
+
+}
+void menu() {
+
+}
 
 int main()
 {
-
-    bool complete1 = false;
-    bool complete2 = false;
-    bool complete3 = false;
-    bool complete4 = false;
-    bool complete5 = false;
-    bool complete6 = false;
-    bool complete7 = false;
-    bool complete8 = false;
-
     system("cls");
     std::cout << "This is Erik Holst's assignment Weekly 2.\n";
     std::cout << "It conatins 8 tasks now. \n";
     std::cout << "Some task are just information on the task or previous task, while task 7 is interactive.\n";
     std::cout << "To choose a task, type a number between 1 and 8, then press enter. \n";
 
+
+    menu();
 
     return 0;
 }
